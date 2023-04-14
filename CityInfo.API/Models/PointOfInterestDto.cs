f19ed4c422e846a5ff4f242045c1ef870a88ca1f
Fredricc <kfred45.fk@gmail.com>
2023-04-14 07:13:39 +0300

    Demo.Returning Resources Part 2

--- CityInfo.API/Models/PointOfInterestDto.cs
+++ CityInfo.API/Models/PointOfInterestDto.cs
@@ -2,5 +2,10 @@
 {
     public class PointOfInterestDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+        
     }
 }
