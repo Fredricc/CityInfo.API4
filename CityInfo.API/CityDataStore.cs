29953c15e7a4b69db164bd4fee80a7106a88fe38
Fredricc <kfred45.fk@gmail.com>
2023-04-15 04:05:01 +0300

    Demo.Returning Child Resources

--- CityInfo.API/CityDataStore.cs
+++ CityInfo.API/CityDataStore.cs
@@ -16,23 +16,75 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "The one with that big park."
+                    Description = "The one with that big park.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id =1,
+                            Name = "Central Park.",
+                            Description = "The most visited urban park in the United States." },
+                         new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building.",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan." },
+                    }
                 },
                  new CityDto()
                 {
                     Id = 2,
                     Name = "Antwerp",
-                    Description = "The one with Cathedral that was never really finished."
+                    Description = "The one with Cathedral that was never really finished.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id =3,
+                            Name = "Cathedral of our Lady.",
+                            Description = "A Gothic style cathedral, conceived by architects Jan and Piete." },
+                         new PointOfInterestDto()
+                        {
+                            Id = 4,
+                            Name = "Antwerp Central Station.",
+                            Description = "The most visited urban park in the United States." },
+                    }
                 }, new CityDto()
                 {
                     Id = 3,
                     Name = "Nakuru",
-                    Description = "The newest city in Kenya that has national park with flamingos."
+                    Description = "The newest city in Kenya that has national park with flamingos.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id =5,
+                            Name = "Lake Elmenteita.",
+                            Description = " A soda lake, in the Great Rift Valley, about 120 km northwest of Nairobi, Kenya." },
+                         new PointOfInterestDto()
+                        {
+                            Id = 6,
+                            Name = "Lake Nakuru National Park",
+                            Description = "On the floor of the Great Rift Valley, surrounded by wooded and bushy grassland" },
+                    }
                 }, new CityDto()
                 {
                     Id = 4,
                     Name = "Eldoret",
-                    Description = "The city were most of Kenya's Air cargo flight land."
+                    Description = "The city were most of Kenya's Air cargo flight land.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id =7,
+                            Name = "Eldoret International Airport.",
+                            Description = "Cargo flight landing zone in Kenya." },
+                         new PointOfInterestDto()
+                        {
+                            Id = 8,
+                            Name = "Uasin Gishu Plateau.",
+                            Description = "west of the Great Rift Valley (in the East African Rift System)." },
+                    }
                 }
             };
         }
--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -5,7 +5,15 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
 
-        public int numberOfPointsOfInterest { get; set; }
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+            = new List<PointOfInterestDto>();
     }
 }
