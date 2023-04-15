49f70b86b427f592e967de5e6207abd7aeccfcd4
Fredricc <kfred45.fk@gmail.com>
2023-04-15 17:49:50 +0300

    Demo.Getting a File

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    public class FilesController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesController(
+            FileExtensionContentTypeProvider fileExtensionContentTypeProvider) 
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider ?? throw new ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+        }
+        
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            var pathToFile = "manipulating-resources-and-validating-input-slides.pdf";
+
+            if(!_fileExtensionContentTypeProvider.TryGetContentType(
+                pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            if  (pathToFile == null)
+            {
+                return NotFound();
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, contentType, Path.GetFileName(pathToFile ));
+        }
+    }
+}
