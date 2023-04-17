261ed5ddc607bd29074542d0af57ac4b16957715
Fredricc <kfred45.fk@gmail.com>
2023-04-17 10:45:39 +0300

    Demo.Logging Exceptions

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -18,6 +18,10 @@ namespace CityInfo.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int  cityId)
         {
+            try
+            {
+                throw new Exception("Exception Sample.");
+
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
 
             if (city == null)
@@ -27,6 +31,15 @@ namespace CityInfo.API.Controllers
             }
 
             return Ok(city.PointsOfInterest);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(
+                    $"Exception while getting points of interest for city with id{cityId}", 
+                    ex);
+                return StatusCode(500, "A problem happened while handling your request.");
+            }
         }
 
         [HttpGet("{pointofinterestid}", Name = "GetPointOfInterest")]
