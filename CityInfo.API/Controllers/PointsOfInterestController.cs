f633203d09568cdde5298f1992b566f0eb38ff45
Fredricc <kfred45.fk@gmail.com>
2023-04-17 10:59:25 +0300

    Demo.Replacing the Default Logger and Logging to a File

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -20,7 +20,7 @@ namespace CityInfo.API.Controllers
         {
             try
             {
-                throw new Exception("Exception Sample.");
+                //throw new Exception("Exception Sample.");
 
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
 
