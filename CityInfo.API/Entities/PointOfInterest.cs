d1495dbbfaa319e5e17f85c1e29d3775559128ba
Fredricc <kfred45.fk@gmail.com>
2023-04-20 19:00:59 +0300

    Demo.Creating Entity Classes

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CityInfo.API.Entities
 {
@@ -7,7 +8,13 @@ namespace CityInfo.API.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+        [ForeignKey("CityId")]
+        public City? City { get; set; }
+        public int CityId { get; set; }
+
         //public string? Description { get; set; }
 
         public PointOfInterest(string name)
