480b2a8d2a390c7fd4001e60a23f3377b70e29fb
Fredricc <kfred45.fk@gmail.com>
2023-04-25 15:41:48 +0300

    Demo.Paging Through Resources

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -13,6 +13,7 @@ namespace CityInfo.API.Controllers
 
         private readonly ICityInfoRepository _cityInfoRespository;
         private readonly IMapper _mapper;
+        const int maxCitiesPageSize = 20;
 
         public CitiesController(ICityInfoRepository cityInfoRespository,
             IMapper mapper)
@@ -22,9 +23,15 @@ namespace CityInfo.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities(string? name, string? searchQuery)
+        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities(string? name, string? searchQuery, int pageNumber = 1, int pageSize = 10)
         {
-            var cityEntities = await _cityInfoRespository.GetCitiesAsync(name, searchQuery);
+            if(pageSize > maxCitiesPageSize)
+            {
+                pageSize = maxCitiesPageSize;
+            }
+
+            var cityEntities = await _cityInfoRespository.GetCitiesAsync(
+                name, searchQuery, pageNumber, pageSize);
 
             return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities));
         }
