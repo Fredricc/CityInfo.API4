f1a7957024f87cd31b5bab0eb8bd465507e99d10
Fredricc <kfred45.fk@gmail.com>
2023-04-28 21:08:48 +0300

    Demo.Adding Authentication Support to Your Documentation

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace CityInfo.API.Controllers
 {
     [Route("api/v{version:apiVersion}/cities/{cityId}/pointsofinterest")]
-    //[Authorize(Policy = "MustBeFromAntwerp")]
+    [Authorize(Policy = "MustBeFromAntwerp")]
     [ApiVersion("1.0")]
     [ApiVersion("2.0")]
     [ApiController]
